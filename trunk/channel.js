579cd3f0a48170fe7609e2588dd5fc47f4d596db
cadmin <cadmin@1f5ed350-1c9c-4149-8d5e-2f899f915510>
2010-05-06 18:20:15 +0000

    oops, forgot to save a couple of files
    git-svn-id: svn+ssh://svn.angstro.com/home/rkhare/svn/nwp@3056 1f5ed350-1c9c-4149-8d5e-2f899f915510

--- trunk/channel.js
+++ trunk/channel.js
@@ -3,20 +3,20 @@
 
 //     Copyright ⓒ 2010 Ångströ, Inc.
 
-//     This file is part of SearchMyStuff.
+//     This file is part of Disco Bridge.
 
-//     SearchMyStuff is free software: you can redistribute it and/or modify
+//     Disco Bridge is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
 //     the Free Software Foundation, either version 3 of the License, or
 //     (at your option) any later version.
 
-//     SearchMyStuff is distributed in the hope that it will be useful,
+//     Disco Bridge is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
 
 //     You should have received a copy of the GNU General Public License
-//     along with SearchMyStuff.  If not, see <http://www.gnu.org/licenses/>.
+//     along with Disco Bridge.  If not, see <http://www.gnu.org/licenses/>.
 
 function Channel() {
     this._observers = [];
