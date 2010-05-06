c2b20a01ca4c046ac1fc1256e70676c79bbb077e
cadmin <cadmin@1f5ed350-1c9c-4149-8d5e-2f899f915510>
2010-05-06 14:30:49 +0000

    added GPLv3 licenses to everything
    git-svn-id: svn+ssh://svn.angstro.com/home/rkhare/svn/nwp@3040 1f5ed350-1c9c-4149-8d5e-2f899f915510

--- trunk/karnak.js
+++ trunk/karnak.js
@@ -1,5 +1,22 @@
 // The Karnak Template Complex
 
+//     Copyright ⓒ 2010 Ångströ, Inc.
+
+//     This file is part of SearchMyStuff.
+
+//     SearchMyStuff is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     SearchMyStuff is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with SearchMyStuff.  If not, see <http://www.gnu.org/licenses/>.
+
 function htmlescape(html) {
     if (!html) return html;
     // This replacement stuff turns out not to be a bottleneck of
--- trunk/local_storage.js
+++ trunk/local_storage.js
@@ -1,3 +1,20 @@
+//     Copyright ⓒ 2010 Ångströ, Inc.
+
+//     This file is part of SearchMyStuff.
+
+//     SearchMyStuff is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     SearchMyStuff is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with SearchMyStuff.  If not, see <http://www.gnu.org/licenses/>.
+
 function LocalStorage(ls) { this._ls = ls; }
 LocalStorage.prototype.get = function(name) { return this._ls[name]; }
 LocalStorage.prototype.set = function(name, value) { this._ls[name] = value; }
